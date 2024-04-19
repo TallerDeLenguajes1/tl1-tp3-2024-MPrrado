79140d412e0a69786dec6eaaf1dcd63b0d592a28
Prado Matias Santiago <matias14prado@gmail.com>
2024-04-19 11:53:20 -0300

    Se agrego codigo en el punto 2

--- ejercicio-3/produccionAnual.c
+++ ejercicio-3/produccionAnual.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include "../funciones.h"
 #include <time.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define ANIOS 5
 #define MESES 12
@@ -8,17 +9,18 @@
 #define LIM_SUP 50000
 
 
-// void cargarMatrizConAleatorios(int matriz[ANIOS][MESES], int lim_inf, int lim_sup);
+void cargarMatrizConAleatorios(int matriz[ANIOS][MESES], int lim_inf, int lim_sup);
 
-// void mostrarMatriz(int matriz[ANIOS][MESES]);
+void mostrarMatriz(int matriz[ANIOS][MESES]);
+void promedioAnual(int matriz[ANIOS][MESES]);
  
 int main ()
 {
     int matriz[ANIOS][MESES];
-    
+    srand(time(NULL));
     cargarMatrizConAleatorios(matriz, LIM_INF, LIM_SUP);
     mostrarMatriz(matriz);
-
+    promedioAnual(matriz);
     //a)
     // for (int i = 0; i < ANIOS; i++)
     // {
@@ -55,106 +57,107 @@ int main ()
     // }
 
     // //d)
-    // int max = 0, min=0, indiceAnioMax, indiceMesMax, indiceAnioMin, indiceMesMin;
-    // for (int i = 0; i < ANIOS; i++)
-    // {
-    //     for (int j = 0; j < MESES; j++)
-    //     {
-    //         if(max < matriz[ANIOS][MESES])
-    //         {
-    //             max = matriz[ANIOS][MESES];
-    //             indiceAnioMax=i+1;
-    //             indiceMesMax=j+1;
-    //         }
-    //         if(min > matriz[ANIOS][MESES])
-    //         {
-    //             min = matriz[ANIOS][MESES];
-    //             indiceAnioMin=i+1;
-    //             indiceMesMin=j+1;
-    //         }
-    //     }
-    // }
+    int max = 0, min=0, indiceAnioMax, indiceMesMax, indiceAnioMin, indiceMesMin;
+    for (int i = 0; i < ANIOS; i++)
+    {
+        for (int j = 0; j < MESES; j++)
+        {
+            if(max < matriz[i][j])
+            {
+                max = matriz[i][j];
+                indiceAnioMax=i;
+                indiceMesMax=j;
+            }
+            if(min > matriz[i][j])
+            {
+                min = matriz[i][j];
+                indiceAnioMin=i;
+                indiceMesMin=j;
+            }
+        }
+    }
 
-    // char anioMax[20],mesMax[20],anioMin[20], mesMin[20];
+    char mesMax[20], mesMin[20];
+    int anioMax, anioMin;
+    switch (indiceAnioMax)
+    {
+    case 0:
+        anioMax = 2019;
+        break;
+    case 1:
+        anioMax = 2020;
+        break;
+    case 2:
+        anioMax = 2021;
+        break;
+    case 3:
+        anioMax = 2022;
+        break;
+    case 4:
+        anioMax = 2023;
+        break;
+    default:
+        break;
+    }
+
+    switch (indiceMesMax)
+    {
+    case 0:
+        strcpy(mesMax, "Enero");
+        break;
+    case 1:
+        strcpy(mesMax, "Febrero");
+        break;
+    case 2:
+        strcpy(mesMax, "Marzo");
+        break;
+    case 3:
+        strcpy(mesMax, "Abril");
+        break;
+    case 4:
+        strcpy(mesMax, "Mayo");
+        break;
+    case 5:
+        strcpy(mesMax, "Junio");
+        break;
+    case 6:
+        strcpy(mesMax, "Julio");
+        break;
+    case 7:
+        strcpy(mesMax, "Agosto");
+        break;
+    case 8:
+        strcpy(mesMax, "Septiembre");
+        break;
+    case 9:
+        strcpy(mesMax, "Octubre");
+        break;
+    case 10:
+        strcpy(mesMax, "Noviembre");
+        break;
+    case 11:
+        strcpy(mesMax, "Diciembre");
+        break;
+    default:
+        break;
+    }
+    
     // switch (indiceAnioMax)
     // {
     // case 0:
-    //     anioMax = "2019";
+    //     anioMin = 2019;
     //     break;
     // case 1:
-    //     anioMax = "2020";
+    //     anioMin = 2020;
     //     break;
     // case 2:
-    //     anioMax = "2021";
+    //     anioMin = 2021;
     //     break;
     // case 3:
-    //     anioMax = "2022";
+    //     anioMin = 2022;
     //     break;
     // case 4:
-    //     anioMax = "2023";
-    //     break;
-    // default:
-    //     break;
-    // }
-
-    // switch (indiceMesMax)
-    // {
-    // case 0:
-    //     mesMax = "Enero";
-    //     break;
-    // case 1:
-    //     mesMax = "Febrero";
-    //     break;
-    // case 2:
-    //     mesMax = "Marzo";
-    //     break;
-    // case 3:
-    //     mesMax = "Abril";
-    //     break;
-    // case 4:
-    //     mesMax = "Mayo";
-    //     break;
-    // case 5:
-    //     mesMax = "Junio";
-    //     break;
-    // case 6:
-    //     mesMax = "Julio";
-    //     break;
-    // case 7:
-    //     mesMax = "Agosto";
-    //     break;
-    // case 8:
-    //     mesMax = "Septiembre";
-    //     break;
-    // case 9:
-    //     mesMax = "Octubre";
-    //     break;
-    // case 10:
-    //     mesMax = "Noviembre";
-    //     break;
-    // case 11:
-    //     mesMax = "Diciembre";
-    //     break;
-    // default:
-    //     break;
-    // }
-    
-    // switch (indiceAnioMin)
-    // {
-    // case 0:
-    //     anioMax = "2019";
-    //     break;
-    // case 1:
-    //     anioMax = "2020";
-    //     break;
-    // case 2:
-    //     anioMax = "2021";
-    //     break;
-    // case 3:
-    //     anioMax = "2022";
-    //     break;
-    // case 4:
-    //     anioMax = "2023";
+    //     anioMin = 2023;
     //     break;
     // default:
     //     break;
@@ -202,34 +205,53 @@ int main ()
     //     break;
     // }
 
-    // printf("La mayor produccion fue \n[%d] \nanio [%s] \nmes [%s]", matriz[indiceAnioMax][indiceMesMax], anioMax, mesMax);
+    printf("\n---------- Mayor produccion anual ----------\n");
+    printf("[%d]\nanio [%d]\nmes [%s]", matriz[indiceAnioMax][indiceMesMax], anioMax, mesMax);
     getchar();
     return 0;
 }
 
-// void cargarMatrizConAleatorios(int matriz[ANIOS][MESES], int lim_inf, int lim_sup)
-// {
-//     for (int i = 0; i < ANIOS; i++)
-//     {
-//         for (int j = 0; j < MESES; j++)
-//         {
-//             matriz[i][j]= rand()%lim_sup + lim_inf;
-//         }
+void cargarMatrizConAleatorios(int matriz[ANIOS][MESES], int lim_inf, int lim_sup)
+{
+    for (int i = 0; i < ANIOS; i++)
+    {
+        for (int j = 0; j < MESES; j++)
+        {
+            matriz[i][j]= rand()%lim_sup + lim_inf;
+        }
         
-//     }
+    }
     
-// }
+}
 
-// void mostrarMatriz(int matriz[ANIOS][MESES])
-// {
-//     printf("\n");
-//     for (int i = 0; i < ANIOS; i++)
-//     {
-//         for (int j = 0; j < MESES; j++)
-//         {
-//             printf("%d ", matriz[i][j]);
-//         }
-//         printf("\n");
-//     }
+void mostrarMatriz(int matriz[ANIOS][MESES])
+{
+    printf("\n");
+    for (int i = 0; i < ANIOS; i++)
+    {
+        for (int j = 0; j < MESES; j++)
+        {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
     
-// }
+}
+
+void promedioAnual(int matriz[ANIOS][MESES])
+{
+    int acumuladorProduccion=0;
+    float promedioAnual=0;
+    printf("--------- PROMEDIOS ANUALES ----------\n");
+    for (int i = 0; i < ANIOS; i++)
+    {
+        for (int j = 0; j < MESES; j++)
+        {
+            acumuladorProduccion = acumuladorProduccion + matriz[i][j];
+        }
+        promedioAnual=(float)acumuladorProduccion/MESES;
+        printf("El promedio del año [%d] es: %.2f\n", i+1, promedioAnual);
+        promedioAnual=0;
+        acumuladorProduccion = 0;
+    }
+}
