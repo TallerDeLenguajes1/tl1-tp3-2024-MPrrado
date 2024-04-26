23a3d72a82a777fa8d756ed8a561a28d4ac136c1
Matias Prado <matias14prado@gmail.com>
2024-04-26 16:43:12 -0300

    se completo el ejercicio 4

--- ejercicio-4/preventistas.c
+++ ejercicio-4/preventistas.c
@@ -5,29 +5,91 @@
 
 #define CANT_MAX_CLIENTES 5
 struct{
-    int ProductoID; //Numerado en ciclo iterativo
-    int Cantidad; // entre 1 y 10
-    char *TipoProducto; // Algún valor del arreglo TiposProductos
-    float PrecioUnitario; // entre 10 - 100
-}typedef producto;
+    int productoID; //Numerado en ciclo iterativo
+    int cantidad; // entre 1 y 10
+    char *TipoProducto; // Algún valor del arreglo tiposProductos
+    float precioUnitario; // entre 10 - 100
+}typedef Producto;
 
-struct{
-    int ClienteID; // Numerado en el ciclo iterativo
+typedef struct cliente{
+    int clienteID; // Numerado en el ciclo iterativo
     char *NombreCliente; // Ingresado por usuario
-    int CantidadProductosAPedir; // (aleatorio entre 1 y 5)
-    producto *Productos //El tamaño de este arreglo depende de la variable
+    int cantidadProductosAPedir; // (aleatorio entre 1 y 5)
+    Producto *productos //El tamaño de este arreglo depende de la variable
     // “CantidadProductosAPedir”
-}typedef cliente;
+}cliente;
 
+
+//funciones
+int solicitarCargaCantidadClientes();
+void cargarDatosCliente(cliente * punteroClientes, int cantidadClientes);
+void mostrarClientes(cliente* punteroClientes, int cantidaClientes);
+void liberarMemoria(cliente* punteroClientes, int cantidadClientes);
+float calcularCostoTotalProducto(Producto producto);
 int main()
+{
+    int cantidadClientes;
+    cliente *punteroClientes = NULL;
+    float precioTotalProducto;
+    srand(time(NULL));
+    int bandera = 0, opcion;
+
+
+    while (bandera == 0)
+    {
+        printf("---------- INICIO SISTEMA PREVENTISTAS ---------\n\n");
+
+        printf("------SELECCIONE UNA OPICION------\n\n");
+        printf("1- Ingresar cantidad clientes\n");
+        printf("2- Cargar datos clientes\n");
+        printf("3- Mostrar datos clientes\n");
+        printf("4- Salir del programa\n");
+        scanf("%d", &opcion);
+
+        switch (opcion)
+        {
+            case 1:
+                cantidadClientes= solicitarCargaCantidadClientes();
+                punteroClientes = (cliente *) malloc(sizeof(cliente) * cantidadClientes);
+                break;
+            case 2:
+                if (punteroClientes == NULL)
+                {
+                    printf("\nERROR NO INGRESO UNA CANTIDAD DE CLIENTES\n");
+                }else
+                {
+                    cargarDatosCliente(punteroClientes, cantidadClientes); //solo se carga nombre lo demas es de manera aleatoria
+                }
+                break;
+            case 3:
+                if (punteroClientes == NULL)
+                {
+                    printf("\nERROR NO HAY INFORMACION PARA MOSTRAR\n");
+                }else
+                {
+                    mostrarClientes(punteroClientes, cantidadClientes);
+                }
+                break;
+            case 4:
+                printf("\nSALIENDO...\n");
+                liberarMemoria(punteroClientes, cantidadClientes);
+                bandera= 1;
+                break;
+            default:
+                printf("\nERROR SELECCIONE UNA OPCION VALIDA\n");
+                break;
+        }
+    }
+    
+    return 0;
+}
+
+
+
+int solicitarCargaCantidadClientes()
 {
     int cantidadClientes = 0;
     cliente *punteroClientes;
-    char buffer[100];
-    char *TiposProductos[]={"Galletas","Snack","Cigarrillos","Caramelos","Bebidas"};
-
-    printf("---------- INICIO SISTEMA PREVENTISTAS ---------\n\n");
-
     printf("Ingrese la cantidad de clientes: ");
     scanf("%d", &cantidadClientes);
     while (cantidadClientes > CANT_MAX_CLIENTES)
@@ -35,15 +97,18 @@ int main()
         printf("Ingrese la cantidad de clientes: ");
         scanf("%d", &cantidadClientes);
     }
-    punteroClientes = (cliente*) malloc(sizeof(cliente) * cantidadClientes);
-    printf("\n---------- Cargue los datos de los clientes ---------- ");
+    return cantidadClientes;
+}
 
-    //carga de datos de cada cliente que se genera
+void cargarDatosCliente(cliente *punteroClientes , int cantidadClientes)
+{
+    char buffer[100];
+    char *tiposProductos[]={"Galletas","Snack","Cigarrillos","Caramelos","Bebidas"};
     for (int i = 0; i < cantidadClientes; i++)
     {
-        punteroClientes[i].ClienteID = i+1;
+        punteroClientes[i].clienteID = i+1;
 
-        printf("Ingrese el nombre del cliente [%d]:", i+1);
+        printf("\n\nIngrese el nombre del cliente [%d]:", i+1);
         fflush(stdin);
         fgets(buffer, 100, stdin);
 
@@ -51,25 +116,68 @@ int main()
 
         strcpy(punteroClientes[i].NombreCliente, buffer);
 
-        punteroClientes[i].CantidadProductosAPedir = rand()%5+1;
+        punteroClientes[i].cantidadProductosAPedir = rand()%5+1;
 
-        punteroClientes[i].Productos = (producto *) malloc(sizeof(producto) * punteroClientes[i].CantidadProductosAPedir);
+        punteroClientes[i].productos = (Producto *) malloc(sizeof(Producto) * punteroClientes[i].cantidadProductosAPedir);
 
-        for (int j = 0; j < punteroClientes[i].CantidadProductosAPedir; j++)
+        int indice;
+        for (int j = 0; j < punteroClientes[i].cantidadProductosAPedir; j++)
         {
-            punteroClientes[i].Productos[j].ProductoID = j+1;
-            punteroClientes[i].Productos[j].Cantidad = rand()%10+1;
-            int indice = rand()%5;
-            punteroClientes[i].Productos[j].TipoProducto = (char *) malloc(sizeof(char) * strlen(TiposProductos[indice]+1));
+            punteroClientes[i].productos[j].productoID = j+100;
+            punteroClientes[i].productos[j].cantidad = rand()%10+1;
+            indice = rand()%5;
+            punteroClientes[i].productos[j].TipoProducto = (char *) malloc(sizeof(char) * strlen(tiposProductos[indice]+1));
         
-            strcpy(punteroClientes[i].Productos[j].TipoProducto, TiposProductos[indice]);
-            puts(punteroClientes[i].Productos[j].TipoProducto);
-            punteroClientes[i].Productos[j].PrecioUnitario = rand()%101+10;
+            strcpy(punteroClientes[i].productos[j].TipoProducto, tiposProductos[indice]);
+            punteroClientes[i].productos[j].precioUnitario =(rand()%10001+1000)/100.00;
         }
         
     }
-    
+}
 
-       
-    return 0;
+void mostrarClientes(cliente *punteroClientes, int cantidadClientes)
+{
+    float precioTotalProducto;
+    float totalPagarCliente =0;
+    printf("\n---------DATOS CLIENTES --------- \n\n");
+    for (int i = 0; i < cantidadClientes; i++)
+    {
+        printf("\n\n------------------DATOS CLIENTE [%d] ------------------ \n\n", i+1);
+        printf("ID CLIENTE: %d\n", (punteroClientes + i)->clienteID );
+        printf("Nombre: %s", (punteroClientes+i)->NombreCliente);
+        printf("Cantidad de productos a pedir: %d", (punteroClientes+i)->cantidadProductosAPedir);
+
+        printf("\n\n\t---Productos y cantidades a pedir del cliente [%d]\n", i+1);
+        for (int j = 0; j < punteroClientes[i].cantidadProductosAPedir; j++)
+        {   
+            printf("\t---Producto [%d]---\n", j+1);
+            printf("\tTipo: %s\n",(punteroClientes + i)->productos[j].TipoProducto);
+            printf("\tLa ID del producto es: %d\n", (punteroClientes + i)->productos[j].productoID);
+            printf("\tLa cantidad de este producto es: %d\n", (punteroClientes + i)->productos[j].cantidad);
+            printf("\tEl precio unitario es: $%.2f\n", (punteroClientes + i)->productos[j].precioUnitario);
+            precioTotalProducto = calcularCostoTotalProducto((punteroClientes + i)->productos[j]);
+            printf("\tEl precio total es: $%.2f\n",precioTotalProducto);
+            totalPagarCliente = totalPagarCliente + precioTotalProducto;
+        }
+
+        printf("\nTotal a pagar del cliente [%d]: $%.2f\n",i+1, totalPagarCliente);
+        totalPagarCliente = 0;
+    }
+}
+
+void liberarMemoria(cliente *punteroClientes, int cantidadClientes)
+{
+    for (int i = 0; i < cantidadClientes; i++)
+    {
+        free(punteroClientes[i].productos);
+    }
+    free(punteroClientes);
+    
+}
+
+float calcularCostoTotalProducto(Producto producto)
+{
+    float precioTotalProducto;
+    precioTotalProducto = producto.cantidad * producto.precioUnitario;
+    return precioTotalProducto;
 }
