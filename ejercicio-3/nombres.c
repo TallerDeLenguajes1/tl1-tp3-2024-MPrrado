f901af4f466bc13b73936d3e27e2de2a1c88279d
Matias Prado <matias14prado@gmail.com>
2024-04-27 22:41:29 -0300

    se modifico el ejercicio 3 para pedir la cantidad de nombres a ingresar

--- ejercicio-3/nombres.c
+++ ejercicio-3/nombres.c
@@ -3,49 +3,54 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define CANT_NOMBRES 5
-void ingresarNombres(char **punteroACadaPunteroDeCadaNombre);
-void mostarNombres(char **punteroACadaPunteroDeCadaNombre);
+// #define CANT_NOMBRES 5
+void ingresarNombres(char **punteroALosPunteroDeCadaNombre, int cantidadNombres);
+void mostarNombres(char **punteroALosPunteroDeCadaNombre, int cantidadNombres);
 int main()
 {
-    char **punteroACadaPunteroDeCadaNombre, *pCadena;
+    char **punteroALosPunteroDeCadaNombre, *pCadena;
+    punteroALosPunteroDeCadaNombre = &pCadena;
+    int cantidadNombres;
+    
+    printf ("----------INICIO PROGRAMA NOMBRES----------\n\n");
+    printf("Ingrese la cantidad de nombres que desea cargar:");
+    scanf("%d", &cantidadNombres);
+    fflush(stdin);
+    punteroALosPunteroDeCadaNombre = (char **) malloc(sizeof(char*) * cantidadNombres);
 
-    punteroACadaPunteroDeCadaNombre = &pCadena;
+    ingresarNombres(punteroALosPunteroDeCadaNombre, cantidadNombres);
+    mostarNombres(punteroALosPunteroDeCadaNombre, cantidadNombres);
 
-    punteroACadaPunteroDeCadaNombre = (char **) malloc(sizeof(char*) * CANT_NOMBRES);
 
-    ingresarNombres(punteroACadaPunteroDeCadaNombre);
-    mostarNombres(punteroACadaPunteroDeCadaNombre);
-
-    for (int i = 0; i < CANT_NOMBRES; i++)
+    for (int i = 0; i < cantidadNombres; i++)
     {
-        free(*(punteroACadaPunteroDeCadaNombre + i));
+        free(*(punteroALosPunteroDeCadaNombre + i));
     }
-    free(punteroACadaPunteroDeCadaNombre);
+    free(punteroALosPunteroDeCadaNombre);
     
     return 0;
 }
 
-void ingresarNombres(char **punteroACadaPunteroDeCadaNombre)
+void ingresarNombres(char **punteroALosPunteroDeCadaNombre, int cantidadNombres)
 {
     char buffer[100];   
     int longitudNombre;
-    printf("---------- INGRESE LOS NOMBRES ----------\n");
-    for (int i = 0; i < CANT_NOMBRES; i++)
+    printf("\n---------- INGRESE LOS NOMBRES ----------\n");
+    for (int i = 0; i < cantidadNombres; i++)
     {
         printf("Nombre [%d]: ", i+1);
         fgets(buffer,100, stdin); //esta funcion la saque de internet, esta bien usarla?
         longitudNombre = strlen(buffer)+1;
-        *(punteroACadaPunteroDeCadaNombre+i) = (char*) malloc(sizeof(char)* longitudNombre); //hacemos la reserva de memoria para cada uno de los punteros que estan dentro del puntero doble para que sea lo justo par cada nombre
-        strcpy(*(punteroACadaPunteroDeCadaNombre+i), buffer);
+        *(punteroALosPunteroDeCadaNombre+i) = (char*) malloc(sizeof(char)* longitudNombre); //hacemos la reserva de memoria para cada uno de los punteros que estan dentro del puntero doble para que sea lo justo par cada nombre
+        strcpy(*(punteroALosPunteroDeCadaNombre+i), buffer);
     }
 }
 
-void mostarNombres(char **punteroACadaPunteroDeCadaNombre)
+void mostarNombres(char **punteroALosPunteroDeCadaNombre, int cantidadNombres)
 {
     printf("\n---------- LOS NOMBRES INGRESADOS SON: ----------\n");
-    for (int i = 0; i < CANT_NOMBRES; i++)
+    for (int i = 0; i < cantidadNombres; i++)
     {
-        printf("El Nombre [%d] es: %s", i+1, punteroACadaPunteroDeCadaNombre[i]);
+        printf("El Nombre [%d] es: %s", i+1, punteroALosPunteroDeCadaNombre[i]);
     }
 }
